0e14e01fbb202b29b1f2e440e294a7baa737716e
Arvind <Arvind@192.168.0.8>
2019-06-04 23:10:57 -0500

    first commit

---
+++ JenTest/src/Test1.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void tc1() {
+		System.out.println("Test 1");
+	}
+	
+	@Test
+	public void tc2() {
+		System.out.println("Test 2");
+	}
+	
+	@Test
+	public void tc3() {
+		System.out.println("Test 3");
+	}
+	
+	@Test
+	public void tc4() {
+		System.out.println("Test 4");
+	}
+
+}
